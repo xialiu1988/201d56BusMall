7168a6c228e98ea1af7214af6332bce6dba2aa6b
xialiu1988 <liuxia198896@gmail.com>
2019-04-08 12:47:58 -0700

    setup files added images

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+var products=[];
+var totalClicks=[];
+var pictureShowIndex=[];
+
+var pictureBox=document.getElementById('pictures');
+var img1=document.getElementById('imgone');
+var img2=document.getElementById('imgtwo');
+var img3=document.getElementById('imgthree');
+
