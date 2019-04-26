86e586c25d2d931057f1cc984bdaa68aeb5ed413
xialiu1988 <liuxia198896@gmail.com>
2019-04-26 10:23:30 -0700

    updated

--- app.js
+++ app.js
@@ -24,26 +24,26 @@ else{
   createinstances();
 }
 function createinstances(){
-  new Product('bag','assets/bag.jpg');
-  new Product('banana','assets/banana.jpg');
-  new Product('bathroom','assets/bathroom.jpg');
-  new Product('boots','assets/boots.jpg');
-  new Product('breakfast','assets/breakfast.jpg');
-  new Product('bubblegum','assets/bubblegum.jpg');
-  new Product('chair','assets/chair.jpg');
-  new Product('cthulhu','assets/cthulhu.jpg');
-  new Product('dog-duck','assets/dog-duck.jpg');
-  new Product('dragon','assets/dragon.jpg');
-  new Product('pen','assets/pen.jpg');
-  new Product('pet-sweep','assets/pet-sweep.jpg');
-  new Product('scissors','assets/scissors.jpg');
-  new Product('shark','assets/shark.jpg');
-  new Product('sweep','assets/sweep.png');
-  new Product('tauntaun','assets/tauntaun.jpg');
-  new Product('unicorn','assets/unicorn.jpg');
-  new Product('usb','assets/usb.gif');
-  new Product('water-can','assets/water-can.jpg');
-  new Product('wine-glass','assets/wine-glass.jpg');
+  new Product('bag','/assets/bag.jpg');
+  new Product('banana','/assets/banana.jpg');
+  new Product('bathroom','/assets/bathroom.jpg');
+  new Product('boots','/assets/boots.jpg');
+  new Product('breakfast','/assets/breakfast.jpg');
+  new Product('bubblegum','/assets/bubblegum.jpg');
+  new Product('chair','/assets/chair.jpg');
+  new Product('cthulhu','/assets/cthulhu.jpg');
+  new Product('dog-duck','/assets/dog-duck.jpg');
+  new Product('dragon','/assets/dragon.jpg');
+  new Product('pen','/assets/pen.jpg');
+  new Product('pet-sweep','/assets/pet-sweep.jpg');
+  new Product('scissors','/assets/scissors.jpg');
+  new Product('shark','/assets/shark.jpg');
+  new Product('sweep','/assets/sweep.png');
+  new Product('tauntaun','/assets/tauntaun.jpg');
+  new Product('unicorn','/assets/unicorn.jpg');
+  new Product('usb','/assets/usb.gif');
+  new Product('water-can','/assets/water-can.jpg');
+  new Product('wine-glass','/assets/wine-glass.jpg');
 }
 
 //createinstances();
